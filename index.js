70a6507561d38359032ccb253af35b0aa90e938b
Dariusz Rosiek <daro.krk@interia.pl>
2018-02-15 22:10:31 +0100

    Create new script in index.js, and new file info.txt

--- index.js
+++ index.js
@@ -1,6 +1,44 @@
 // informacje o obrazku
 
+/*
 var fs = require('fs');
+var colors = require('colors');
+
+fs.readFile('./tekst.txt', 'utf-8', function(err, data) {
+	console.log('Dane przed zapisem!'.blue);
+	console.log(data);
+	fs.appendFile('./tekst.txt', '\nA tak wyglądają po zapisie!', function(err) {
+		if (err) throw err;
+		console.log('Zapisano!'.blue);
+		fs.readFile('./tekst.txt', 'utf-8', function(err, data) {
+			console.log('Dane po zapisie'.blue);
+			console.log(data);
+		});
+	});
+});
+*/
+//-----------------------------------------------------------//
+
+//odczytanie plików folderu i napisanie ich w nowym pliku
+
+var fs = require('fs');
+var colors = require('colors');
+
+fs.readdir('.', 'utf-8', function(err, files) {
+	console.log('Pliki katalogu zadanie_13_7:'.green);
+	console.log(files);
+
+	fs.writeFile('./info.txt', files, function(err) {
+	if (err) throw err;
+	console.log('Zapisano w pliku info.txt!'.red);
+	});
+});
+
+
+
+
+
+/*
 var StatMode = require('stat-mode');
 
 fs.stat('./cat.jpg', function(err, stats) {
@@ -8,3 +46,12 @@ fs.stat('./cat.jpg', function(err, stats) {
 	console.log(statMode.toString());
 });
 
+fs.readFile('./tekst.txt', 'utf-8', function(err, data) {
+	console.log(data);
+});
+
+fs.writeFile('./tekst.txt', 'Tekst, który zapiszemy w pliku tekst.txt', function(err) {
+    if (err) throw err; // jeśli pojawi się błąd, wyrzuć wyjątek
+    console.log('Zapisano!');
+});
+*/
