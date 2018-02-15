380b43f2386e875ad78bcbc80dd618712e5a93ba
Dariusz Rosiek <daro.krk@interia.pl>
2018-02-15 19:38:57 +0100

    Install stat-mode and added script

---
+++ index.js
@@ -0,0 +1,10 @@
+// informacje o obrazku
+
+var fs = require('fs');
+var StatMode = require('stat-mode');
+
+fs.stat('./cat.jpg', function(err, stats) {
+	var statMode = new StatMode(stats);
+	console.log(statMode.toString());
+});
+
